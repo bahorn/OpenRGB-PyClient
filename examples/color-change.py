b734912ecccdf2c7dc36f9dfefd5f750158f6d86
B Horn <b@horn.uk>
2020-05-12 14:43:31 +0100

    move examples to another dir, added requirements.txt

---
+++ examples/color-change.py
@@ -0,0 +1,26 @@
+import time
+import sys,os
+
+sys.path.append(os.path.realpath('.'))
+from openrgb import OpenRGB
+
+
+client = OpenRGB('localhost', 1337)
+count = client.controller_count()
+devices = {}
+for i in range(count):
+    devices[i] = client.controller_data(device_id=i)
+    led_count = len(devices[i].leds)
+    client.update_leds([0x00]*led_count, device_id=i)
+
+
+val = 0
+while True:
+    val += 1
+    i = 0
+    for i in range(count):
+        cur = devices[i]
+        led_count = len(cur.leds)
+        client.update_leds([0x2f*val]*led_count, device_id=i)
+
+    time.sleep(1)
