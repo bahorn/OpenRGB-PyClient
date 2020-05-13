fd9fd4143791f8f9b5d0bab13ce27b298c10b068
B Horn <b@horn.uk>
2020-05-13 07:39:09 +0100

    added example to list modes supported by each device

---
+++ examples/get-modes.py
@@ -0,0 +1,14 @@
+import time
+import sys,os
+
+sys.path.append(os.path.realpath('.'))
+from openrgb import OpenRGB
+
+client = OpenRGB('localhost', 1337)
+count = client.controller_count()
+for i in range(count):
+    device = client.controller_data(device_id=i)
+    print('{} - {}'.format(device.name, device.type))
+    for mode in device.modes:
+        print('* {}'.format(mode['name']))
+    print()
