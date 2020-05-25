29438c96ae8db972bad74521ff558e5e37087b94
B Horn <b@horn.uk>
2020-05-25 03:39:33 +0100

    Resolving #1 by resolving the last char if it's a null byte.

--- openrgb/binreader.py
+++ openrgb/binreader.py
@@ -2,10 +2,11 @@ import struct
 
 # Allows extracting of values from binary blobs
 class Blob:
- 
+
     def __init__(self, data, idx=0):
         self.data = data
         self.idx = idx
+
     def _unpack(self, fmt):
         size = struct.calcsize(fmt)
         res = struct.unpack(fmt, self.data[self.idx: self.idx + size])
@@ -14,16 +15,22 @@ class Blob:
 
     def ushort(self):
         return self._unpack('H')[0]
+
     def uint(self):
         return self._unpack('I')[0]
+
     def int(self):
         return self._unpack('i')[0]
+
     def color(self):
         return self._unpack('cccx')
+
     def string(self):
         total_len = self.ushort()
-        return self._unpack(
+        unpacked = self._unpack(
             '{}s'.format(total_len)
         )[0].decode('ascii')
+        return unpacked.strip('\x00')
+
     def skip(self, count):
         self._unpack('x'*count)
